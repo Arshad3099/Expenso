cff4bbf0d3a3e38cb41ff5e985f1e8cadb95e156
Arshad3099 <arshadbasha1999@outlook.in>
2024-07-23 23:19:37 +0530

    Initial commit

---
+++ src/main/java/com/expenso/tracker/ExpensoApplication.java
@@ -0,0 +1,13 @@
+package com.expenso.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expenso/tracker/ExpensoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenso.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
